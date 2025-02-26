697775ad8663cfff528caf5b712879df42f72e90
celarim <k6jun@kookmin.ac.kr>
2025-02-26 14:37:38 +0900

    [Feat] BoradDetail 화면 구성
    댓글 컴포넌트 만들기
    댓글 불러오기
    상세페이지 화면 구성
    댓글 작성 기능 만들기

---
+++ src/board/component/BoardComment.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+const props = defineProps({
+    comment: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+<template>
+    <div style="display:flex">
+        <div>
+            {{ comment.writer }} : 
+        </div>
+        <div>
+            {{ comment.content }}
+        </div>
+    </div>
+
+</template>
+
+<style scoped></style>
